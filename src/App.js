44cdd3464f2ba70bc84a1fa0bacabac0f9af9610
Julien Deswaef <judeswae@thoughtworks.com>
2017-10-01 13:11:53 -0400

    React Primitives

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import { Container, Header } from 'semantic-ui-react';
+//import { Container, Header } from 'semantic-ui-react';
 import { Profile } from 'xuv-components';
-import 'semantic-ui-css/semantic.min.css';
+//import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Container className="column" >
+      <div>
         <Profile user = "xuv" />
-      </Container>
+      </div>
     );
   }
 }
